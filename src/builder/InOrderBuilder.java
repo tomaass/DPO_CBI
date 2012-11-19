fdc3db2bb3c1076ecd347871e74fcfcda9c38530
Maty <Maty@MATY-HP-W8>
2012-11-19 22:08:34 +0100

    Commit 1

---
+++ src/builder/InOrderBuilder.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package builder;
+
+/**
+ *
+ * @author Maty
+ */
+public class InOrderBuilder extends ExpressionBuilder {
+    
+}
